b1a1193820b5bf39a456dee2cc1b31b838fa3ded
lauragarcia <lauragarcia@uma.es>
2019-04-26 12:01:08 +0200

    Refactor class Factorial

--- Factorial/src/main/java/org/uma/pab19/Factorial.java
+++ Factorial/src/main/java/org/uma/pab19/Factorial.java
@@ -1,7 +1,26 @@
 package org.uma.pab19;
 
+/**
+ * Class containing a method to compute the factorial of an integer number
+ *
+ * @author L.Garcia
+ */
 public class Factorial {
-    public int compute(int number) {
-        return 1;
+    /**
+     * @param number
+     * @return The factorial of an integer number
+     */
+    public double compute(int number) {
+        double result;
+
+        if (number < 0) {
+            throw new RuntimeException("The value is negative" + number);
+
+        } else if ((number == 0) || (number == 1)) {
+            result = 1;
+        } else {
+            result = number * compute(number - 1);
+        }
+        return result;
     }
 }
--- Factorial/src/test/java/org/uma/pab19/FactorialTest.java
+++ Factorial/src/test/java/org/uma/pab19/FactorialTest.java
@@ -1,23 +1,67 @@
 package org.uma.pab19;
+
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FactorialTest {
+    private static final double EPSILON = 0.000000001;
+    Factorial factorial;
+
+    @Before
+    public void setup() {
+        factorial = new Factorial();
+    }
+
+    @After
+    public void after() {
+        factorial = null;
+    }
+
     @Test
     public void shouldFactorialOfZeroReturnOne() {
-        int expectedResult=1;
-        Factorial factorial=new Factorial();
-        int obtainedResult=factorial.compute(0);
+        double expectedResult = 1;
+        double obtainedResult = factorial.compute(0);
 
-        assertEquals(expectedResult,obtainedResult);
+        assertEquals(expectedResult, obtainedResult, EPSILON);
     }
 
     @Test
     public void shouldFactorialOfOneReturnOne() {
-        int expectedResult=1;
-        Factorial factorial=new Factorial();
-        int obtainedResult=factorial.compute(1);
+        double expectedResult = 1;
+        double obtainedResult = factorial.compute(1);
 
-        assertEquals(expectedResult,obtainedResult);
+        assertEquals(expectedResult, obtainedResult, EPSILON);
+    }
+
+    @Test
+    public void shouldFactorialOfTwoReturnOne() {
+        double expectedResult = 2;
+        double obtainedResult = factorial.compute(2);
+
+        assertEquals(expectedResult, obtainedResult, EPSILON);
+    }
+
+    @Test
+    public void shouldFactorialOfThreeReturnOne() {
+        double expectedResult = 6;
+        double obtainedResult = factorial.compute(3);
+
+        assertEquals(expectedResult, obtainedResult, EPSILON);
+    }
+
+    @Test
+    public void shouldFactorialOfFourReturnOne() {
+        double expectedResult = 24;
+        double obtainedResult = factorial.compute(4);
+
+        assertEquals(expectedResult, obtainedResult, EPSILON);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldFactorialThrowsAnExceptionIfTheValueIsNegative() {
+        factorial.compute(-2);
     }
 }
